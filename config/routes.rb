326910ec29bc4e50ed5039420d58945dffe1ac43
Timothy Vieira <vieira.timothy@yahoo.ca>
2020-08-12 15:03:13 -0400

    Added routes and controllers

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  resources :restaurants, only: [:new, :create, :index, :show] do
+    resources :reviews, only: [:new, :create, :index, :show]
+  end
+  resources :reviews, only: [:new, :create, :index, :show]
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,20 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+puts "Cleaning database..."
+Restaurant.destroy_all
+
+puts "Creating restaurants..."
+
+dishoom = { name: "Dishoom", address: "7 Boundary St, London E2 7JE", category: "chinese" }
+pizza_east = { name: "Pizza East", address: "56A Shoreditch High St, London E1 6PQ", category: "italian" }
+dolcetto = { name: "Dolcetto", address: "3020 Colonel Talbot Rd, London", category: "italian" }
+hungry_ninja = { name: "Hungry Ninja", address: "2208 Piccadilly St, London, ON N6A 1S1", category: "japanese" }
+davids_bistro = { name: "David's Bistro", address: "432 Richmond St, London, ON N6A 3C9", category: "belgian" }
+
+[dishoom, pizza_east, dolcetto, hungry_ninja, davids_bistro].each do |attributes|
+  restaurant = Restaurant.create(attributes)
+  puts "Created #{restaurant.name}"
+end
+puts "Finished!"
