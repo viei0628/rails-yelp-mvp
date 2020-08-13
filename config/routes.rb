4860c6bc858e4dadd8e91dae299000b6a9d22b01
Timothy Vieira <vieira.timothy@yahoo.ca>
2020-08-12 20:31:54 -0400

    redirected or rendered

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  resources :restaurants, only: [:new, :create, :index, :show] do
-    resources :reviews, only: [:new, :create, :index, :show]
+  resources :restaurants, only: [:index, :new, :create, :show] do
+    resources :reviews, only: [:index, :new, :create, :show]
   end
-  resources :reviews, only: [:new, :create, :index, :show]
+  # resources :reviews, only: [:new, :create, :index, :show]
 end
